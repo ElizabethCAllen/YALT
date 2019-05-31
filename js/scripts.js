fb6f067f2379276f175039ac47b7e6e0f27d6361
Ana uyehara <ana.uyehara0@gmail.com>
2019-05-30 21:30:28 -0700

    started building tip calc

---
+++ js/scripts.js
@@ -0,0 +1,44 @@
+// {/* <script> */}
+
+function calc() {
+
+    var bill = Number(document.getElementById('bill').value);
+    var tip = bill * .15;
+    var total_bill = bill + tip;
+    document.getElementById("tip").innerHTML= "$"+Number(tip).toFixed(2);
+    document.getElementById("total").innerHTML= "$"+Number(total_bill).toFixed(2);        
+
+}
+
+// </script>
+    var myBtn = document.getElementById('my-button');
+    var myBtn2 = document.getElementById('my-button2');
+    var tipElem = document.getElementById("tip");
+    var totalElem = document.getElementById("total");
+    var tiprate = document.getElementById("tiprate");
+    myBtn.addEventListener('click', calc);
+    myBtn2.addEventListener('click', calc2);
+
+function calc(e) {
+  e.preventDefault();
+  var input = document.getElementById('bill');
+  var inputTip = document.getElementById('tippercent');
+  var bill = parseFloat(input.value); 
+  var tippercent = parseFloat(inputTip.value);
+  var tip = bill * (tippercent/100);
+  var total = bill + tip; 
+  tipElem.textContent = "$"+(tip).toFixed(2);
+  totalElem.textContent = "$"+(total).toFixed(2); 
+}
+
+function calc2(e) {
+  e.preventDefault();
+  var input = document.getElementById('bill');
+  var input2 = document.getElementById('tipamt');
+  var bill = parseFloat(input.value);     
+  var tip = parseFloat(input2.value);  
+  var total = bill + tip; 
+  totalElem.textContent = "$"+(total).toFixed(2);    
+  var tippercent = parseFloat(tip/(bill)*100);
+  tiprate.textContent = (tippercent).toFixed(2);
+}
